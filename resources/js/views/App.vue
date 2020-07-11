5e41c394ba0383ae0c673493b5f3e67edf435094
Daria <54178803+Daria0304@users.noreply.github.com>
2020-07-11 16:32:45 +0200

    Initial commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <nav class="w-full mb-4 p-4 bg-gray-700 text-white">
+      <router-link to="/" class="hover:text-blue-500">Home</router-link>
+      <router-link to="/about" class="ml-4 hover:text-blue-500">About</router-link>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {};
+</script>
